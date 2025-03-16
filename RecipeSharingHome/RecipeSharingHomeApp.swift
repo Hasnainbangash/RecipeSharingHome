738c1e962d697179068d1927a0694eb65d21b319
Bangash38 <bangashmhasnain38@gmail.com>
2025-03-16 16:21:24 +0500

    Initial Commit

---
+++ RecipeSharingHome/RecipeSharingHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeSharingHomeApp.swift
+//  RecipeSharingHome
+//
+//  Created by Muhammad Hasnain Bangash on 16/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct RecipeSharingHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RecipeSharingHomeTests/RecipeSharingHomeTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeSharingHomeTests.swift
+//  RecipeSharingHomeTests
+//
+//  Created by Muhammad Hasnain Bangash on 16/03/2025.
+//
+
+import Testing
+@testable import RecipeSharingHome
+
+struct RecipeSharingHomeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
