48a68508a0c49a89460e5b1a3f33a0a334e2d7b4
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-17 10:23:40 +0500

    REciep item view wrok alomost work done

--- RecipeSharingHome/View/RecipeItemView.swift
+++ RecipeSharingHome/View/RecipeItemView.swift
@@ -10,22 +10,43 @@ import SwiftUI
 struct RecipeItemView: View {
     var body: some View {
         VStack(spacing: 15) {
+            // FOOD IMAGE
             Image("Food1")
                 .resizable()
                 .scaledToFit()
                 .frame(height: 167)
                 .overlay(
-                    Image(systemName: "trash")
-                        .foregroundColor(Color.red)
-                        .padding()
+                    Button(action: {
+                        print("Delete button tapped")
+                    }) {
+                        Image(systemName: "trash")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 16, height: 16)
+                            .foregroundColor(.red)
+                            .padding(8)
+                            .background(Color.white)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            .shadow(radius: 2)
+                    }
+                        .frame(width: 32, height: 32)
+                        .offset(x: -10, y: 10)
                     , alignment: .topTrailing
                 )
                 .overlay(
                     Text("10 Mint")
+                        .font(.system(size: 14, weight: .medium))
                         .foregroundColor(Color("ColorDarkGreen"))
-                        .padding()
+                        .padding(.horizontal, 10)
+                        .padding(.vertical, 4)
+                        .background(Color.white)
+                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                        .shadow(radius: 2)
+                        .offset(x: 10, y: -10)
                     , alignment: .bottomLeading
                 )
+            
+            // PROFILE
             
             HStack(alignment: .top) {
                 Image("ProfileImage1")
@@ -54,6 +75,21 @@ struct RecipeItemView: View {
                 .padding(.horizontal ,4)
                 .background(Color("ColorLightGreen"))
                 .cornerRadius(8)
+            } //: HSTACK
+            
+            HStack(alignment: .center) {
+                Spacer()
+                Image(systemName: "heart")
+                    .font(.title3)
+                Spacer()
+                Image(systemName: "ellipsis.message")
+                    .font(.title3)
+                Spacer()
+                Image(systemName: "bookmark")
+                    .font(.title3)
+                Spacer()
+                Image(systemName: "square.and.arrow.up")
+                    .font(.title3)
             } //: HSTACK
         } //: VSTACK
     }
