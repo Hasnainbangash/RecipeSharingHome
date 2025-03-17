19c9597f3589bdf421518eaa06646b3aac1a0e3f
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-17 11:54:09 +0500

    Some issues get resolved and more work done

--- RecipeSharingHome/View/CategoryView.swift
+++ RecipeSharingHome/View/CategoryView.swift
@@ -8,17 +8,40 @@
 import SwiftUI
 
 struct CategoryView: View {
+    // MARK: - PROPERTIES
+    @State private var selectedCategory: String = "Breakfast"
+    
+    let categories = [
+        "Breakfast",
+        "Lunch",
+        "Drinks",
+        "Appetizer"
+    ]
+    
+    // MARK: - BODY
     var body: some View {
         ScrollView(.horizontal, showsIndicators: false) {
             HStack(alignment: .center, spacing: 10) {
-                CategoriesItemView(categoryText: "Breakfast", categoryColor: "ColorDarkGreen")
-                CategoriesItemView(categoryText: "Lunch", categoryColor: "ColorLightGreen")
-                CategoriesItemView(categoryText: "Drinks", categoryColor: "ColorLightGreen")
-                CategoriesItemView(categoryText: "Appetizer", categoryColor: "ColorLightGreen")
+                ForEach(categories, id: \.self) { category in
+                    Button(action: {}) {
+                        Text(category)
+                            .fontWeight(.semibold)
+                            .padding(.horizontal, 20)
+                            .padding(.vertical, 10)
+                            .background(selectedCategory == category ? Color("ColorDarkGreen") : Color("ColorLightGreen"))
+                            .foregroundColor(Color.white)
+                            .cornerRadius(10)
+                            .onTapGesture {
+                                selectedCategory = category
+                            }
+                    } //: BUTTON
+                } //: LOOP
             } //: HSTACK
         } //: SCROLL VIEW
     }
 }
+
+// MARK: - PREVIEW
 
 #Preview {
     CategoryView()
